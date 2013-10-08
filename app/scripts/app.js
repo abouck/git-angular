8aaf9812c0d56171a7a369eb5788a8d3a7a38b27
Adam Bouck <Adam.j.Bouck@gmail.com>
2013-10-08 14:53:20 -0700

    implemented

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('angularIssuesApp', [])
+angular.module('angularIssuesApp', ['ngResource'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,20 @@
 'use strict';
 
 angular.module('angularIssuesApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, $resource) {
   	
+  	//setup
+  	var Issue = $resource('https://api.github.com/repos/abouck/test_1/issues/:number',
+  		{number: '@number'});
 
+
+  
     $scope.loadIssues = function() {
       $http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode($scope.username + ':' + $scope.password);
-      $http.get('https://api.github.com/repos/abouck/test_1/issues').success(function(response){
-      	$scope.issues = response      	
-      })
+      	$scope.issues = Issue.query();      	
     };
 
-    $scope.addIssue = function(title,body) {
+    $scope.addIssue = function() {
     	$http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode($scope.username + ':' + $scope.password);
     	var data = {title: $scope.title,body: $scope.body}
     	$http.post('https://api.github.com/repos/abouck/test_1/issues', data ).success(function(response){
@@ -22,7 +25,11 @@ angular.module('angularIssuesApp')
 
     $scope.updateState = function(issue){
     	issue.state=(issue.state == "open" ? "closed" : "open")
-       	$http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode($scope.username + ':' + $scope.password);
-    	$http.post(issue.url, issue).success(function(response){console.log(response)})
+     //   	$http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode($scope.username + ':' + $scope.password);
+    	// $http.post(issue.url, issue).success(function(response){console.log(response)})
+    	// console.log(issue)
+
+    	issue.$save()
     }
+
   });
